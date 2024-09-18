a78d6e34b6ea684318e2506dd69700bfe92a5290
jj1980a <j.feral@live.fr>
2024-09-18 09:50:44 +0400

    GetDAppSolverGasLimit method

--- core/dapp_control.go
+++ core/dapp_control.go
@@ -1,18 +1,29 @@
 package core
 
 import (
+	"math/big"
+
 	"github.com/FastLane-Labs/atlas-sdk-go/contract/dappcontrol"
 	"github.com/FastLane-Labs/atlas-sdk-go/types"
 	"github.com/ethereum/go-ethereum/common"
 )
 
-func (sdk *AtlasSdk) GetDAppCallConfig(chainId uint64, dAppControlAddr common.Address) (uint32, error) {
+func (sdk *AtlasSdk) getDAppControlContract(chainId uint64, dAppControlAddr common.Address) (*dappcontrol.DAppControl, error) {
 	ethClient, err := sdk.getEthClient(chainId)
 	if err != nil {
-		return 0, err
+		return nil, err
 	}
 
 	dAppControlContract, err := dappcontrol.NewDAppControl(dAppControlAddr, ethClient)
+	if err != nil {
+		return nil, err
+	}
+
+	return dAppControlContract, nil
+}
+
+func (sdk *AtlasSdk) GetDAppCallConfig(chainId uint64, dAppControlAddr common.Address) (uint32, error) {
+	dAppControlContract, err := sdk.getDAppControlContract(chainId, dAppControlAddr)
 	if err != nil {
 		return 0, err
 	}
@@ -26,12 +37,7 @@ func (sdk *AtlasSdk) GetDAppCallConfig(chainId uint64, dAppControlAddr common.Ad
 }
 
 func (sdk *AtlasSdk) GetDAppConfig(chainId uint64, userOp *types.UserOperation, dAppControlAddr common.Address) (*dappcontrol.DAppConfig, error) {
-	ethClient, err := sdk.getEthClient(chainId)
-	if err != nil {
-		return nil, err
-	}
-
-	dAppControlContract, err := dappcontrol.NewDAppControl(dAppControlAddr, ethClient)
+	dAppControlContract, err := sdk.getDAppControlContract(chainId, dAppControlAddr)
 	if err != nil {
 		return nil, err
 	}
@@ -42,4 +48,18 @@ func (sdk *AtlasSdk) GetDAppConfig(chainId uint64, userOp *types.UserOperation, 
 	}
 
 	return &dAppConfig, nil
+}
+
+func (sdk *AtlasSdk) GetDAppSolverGasLimit(chainId uint64, dAppControlAddr common.Address) (*big.Int, error) {
+	dAppControlContract, err := sdk.getDAppControlContract(chainId, dAppControlAddr)
+	if err != nil {
+		return nil, err
+	}
+
+	solverGasLimit, err := dAppControlContract.GetSolverGasLimit(nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return new(big.Int).SetUint64(uint64(solverGasLimit)), nil
 }
